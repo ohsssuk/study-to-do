d1bf9e2c42f1c861c418ccaca3c12366c47c8154
ohsssuk <ohsssuk@naver.com>
2024-06-30 03:36:46 +0900

    로딩 화면 추가

--- src/containers/ToDo/index.tsx
+++ src/containers/ToDo/index.tsx
@@ -5,29 +5,41 @@ import useTodoStore from "@/stores/todo-store";
 import CommonBtn from "@/components/ui/CommonBtn";
 import { useRouter } from "next/navigation";
 import TodoItem from "./TodoItem";
+import Loading from "@/components/ui/Loading";
 
 export default function ToDo() {
   const router = useRouter();
 
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [isError, setIsError] = useState<boolean>(false);
 
   const { todoItems, createTodo } = useTodoStore();
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, [todoItems]);
 
   const handleClickToEdit = () => {
     router.push("/edit");
   };
 
   return (
-    <div id={styles.todo_list}>
+    <>
       <div className={styles.cta}>
         <CommonBtn onClick={handleClickToEdit}>추가 버튼</CommonBtn>
       </div>
-      <ul className={styles.list}>
-        {todoItems.map((todo, index) => (
-          <TodoItem todo={todo} index={index} />
-        ))}
-      </ul>
-    </div>
+      <div id={styles.todo_list}>
+        {isLoading ? (
+          <Loading />
+        ) : todoItems.length === 0 ? (
+          <p>일정이 없습니다.</p>
+        ) : (
+          <ul className={styles.list}>
+            {todoItems.map((todo, index) => (
+              <TodoItem todo={todo} index={index} />
+            ))}
+          </ul>
+        )}
+      </div>
+    </>
   );
 }
