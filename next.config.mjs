9a4a4394cb84c68f81149af4d28537f3996009a8
ohsssuk <ohsssuk@naver.com>
2024-08-15 23:46:10 +0900

    next config 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  output: "export",
+  // basePath 명시
+  basePath: isProduction ? "/study-to-do" : "",
+  // next 이미지 최적화 옵션 off
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://ohsssuk.github.io/study-to-do"
+      : "",
+};
 
 export default nextConfig;
